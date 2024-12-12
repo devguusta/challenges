59e285d163fe3e85dd300892df60ed0b7f15796e
Gustavo Silva <gustavodev60@gmail.com>
2024-12-11 21:32:28 -0300

    create twosum logic

--- lib/twosum/twosum.dart
+++ lib/twosum/twosum.dart
@@ -1,12 +1,15 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    List<int> result = [];
-    for (var i = 0; i < nums.length; i++) {
-      if (nums[i] + nums[i + 1] == target) {
-        result.addAll([i, i + 1]);
-        break;
+    Map<int, dynamic> hashMap = {};
+    int i = 0;
+    for (int n in nums) {
+      int val = target - n;
+      if (hashMap.containsKey(n)) {
+        return [hashMap[n], i];
       }
+      hashMap.addAll({val: i});
+      i++;
     }
-    return result;
+    return [];
   }
 }
